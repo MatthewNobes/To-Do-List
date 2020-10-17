833f3ebe9cba71752027dfddf81bf388fc27914f
MatthewNobes <mattnobes.personal@protonmail.com>
2020-10-17 16:30:57 +0100

    conent update

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './../App.css';
+
+function generateItemList() {
+    return (
+        <ul>
+            <li>num 1</li>
+            <li>num 2</li>
+            <li>num 3</li>
+        </ul>
+    );
+}
 
 function MainForm() {
-    return <h1 className="Main Title">hello</h1>;
+    return (
+        <div>
+            <h1 
+            className="MainTitle">
+                To-Do List
+            </h1>
+            {generateItemList()}
+            
+        </div>
+        
+    );
 }
 
 export default MainForm;
